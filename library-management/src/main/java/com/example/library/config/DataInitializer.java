4670f693d53420a64a4a904b629f59a2f5e5e146
Kacper Szabat <kacper.szabat99@gmail.com>
2024-07-26 16:33:56 +0200

    The User Role is not working correctly

--- library-management/src/main/java/com/example/library/config/DataInitializer.java
+++ library-management/src/main/java/com/example/library/config/DataInitializer.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
--- library-management/src/main/java/com/example/library/service/impl/UserServiceImpl.java
+++ library-management/src/main/java/com/example/library/service/impl/UserServiceImpl.java
@@ -6,16 +6,9 @@ import com.example.library.model.User;
 import com.example.library.repository.RoleRepository;
 import com.example.library.repository.UserRepository;
 import com.example.library.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Set;
 
 @Service
