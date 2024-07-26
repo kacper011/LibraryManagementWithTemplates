50340c0f14aa0424c4c1c894e57465b42043883d
Kacper Szabat <kacper.szabat99@gmail.com>
2024-07-26 15:53:40 +0200

    Role Admin is working

--- library-management/src/main/java/com/example/library/controller/BooksController.java
+++ library-management/src/main/java/com/example/library/controller/BooksController.java
@@ -1,9 +1,10 @@
-package com.example.library.controller.admin_controller;
+package com.example.library.controller;
 
 import com.example.library.model.Book;
 import com.example.library.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,28 +13,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class AdminController {
+public class BooksController {
 
 
     private BookService bookService;
 
-    public AdminController(BookService bookService) {
+    public BooksController(BookService bookService) {
         this.bookService = bookService;
     }
 
     @GetMapping("/books")
-    public String getAllBooks(Model model) {
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public String getAllBooks(Model model, Authentication authentication) {
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
-        return "admin/books";
+
+        boolean hasAdminRole = authentication.getAuthorities()
+                .stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMIN"));
+        boolean hasUserRole = authentication.getAuthorities()
+                .stream()
+                .anyMatch(auth -> auth.getAuthority().equals("ROLE_USER"));
+
+        if (hasAdminRole) {
+            return "books_admin";
+        } else if (hasUserRole) {
+            return "books_user";
+        } else {
+            return "access_denied";
+        }
     }
+
+    @GetMapping("/books_admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getBooksAdmin(Model model) {
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books", books);
+        return "books_admin";
+    }
+
+
+
 
     @GetMapping("/books/new")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String addBook(Model model) {
         Book book = new Book();
         model.addAttribute("book", book);
-        return "admin/create_book";
+        return "create_book_admin";
     }
 
     @PostMapping("/books")
@@ -46,14 +73,14 @@ public class AdminController {
         }
 
         bookService.createBook(book);
-        return "redirect:/books";
+        return "redirect:/books_admin";
     }
     @GetMapping("/books/{id}/edit")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String editBook(@PathVariable("id") Long id, Model model) {
         Book book = bookService.getBookById(id);
         model.addAttribute("book", book);
-        return "admin/edit_book";
+        return "edit_book_admin";
     }
 
     @PostMapping("/books/{id}")
@@ -61,7 +88,7 @@ public class AdminController {
     public String updateBook(@PathVariable("id") Long id,@Valid @ModelAttribute Book book, BindingResult result, Model model) {
         if (result.hasErrors()) {
             model.addAttribute("books", book);
-            return "edit_book";
+            return "edit_book_admin";
         }
 
         book.setId(id);
