2be459ddb9f1caf83eaea4902d5f9327274d302e
Kacper Szabat <kacper.szabat99@gmail.com>
2024-09-13 16:26:48 +0200

    Writing tests for the CustomErrorController class

---
+++ library-management/src/test/java/com/example/library/controller/CustomErrorControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.library.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(CustomErrorController.class)
+class CustomErrorControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void testHandlerError() throws Exception {
+
+        mockMvc.perform(get("/error"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testGetErrorPath() {
+
+        CustomErrorController customErrorController = new CustomErrorController();
+        String path = customErrorController.getErrorPath();
+        assertEquals("/error", path, "The error path should be /error");
+    }
+}
