eb571ab139488493def90f3395bd9c17e0ee61ae
Kacper Szabat <kacper.szabat99@gmail.com>
2025-03-14 18:49:43 +0100

    Writing a login function in cypress

--- library-management/cypress/e2e/homepage as user/return_a_book.cy.js
+++ library-management/cypress/e2e/homepage as user/return_a_book.cy.js
@@ -1,12 +1,6 @@
 describe('Return a book', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:8080/login');
-
-        cy.get('.login-form').should('exist');
-        cy.get('#username').type('Kacper11');
-        cy.get('#password').type('kacper11');
-        cy.get('#btn-login').click();
-
+        cy.login();
         cy.url().should('eq', 'http://localhost:8080/books_user');
     });
 
--- library-management/cypress/support/commands.js
+++ library-management/cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+    cy.visit('http://localhost:8080/login');
+
+    cy.get('.login-form').should('exist');
+    cy.get('#username').type('Kacper11');
+    cy.get('#password').type('kacper11');
+    cy.get('#btn-login').click();
+
+    cy.url().should('eq', 'http://localhost:8080/books_user');
+});
